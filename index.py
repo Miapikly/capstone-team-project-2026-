1e13b1dd1c19217a617b9f7e10a2dad22d479020
Jessica Quezada <jqueza755@west-mec.org>
2026-05-12 17:29:12 -0700

    update

--- index.py
+++ index.py
@@ -2,9 +2,14 @@ import pygame
 import spritesheet
 
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
 
-sprite_sheet_image = pygame.image.load('doux.png').convert_alpha()
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption('Spritesheets')
+
+sprite_sheet_image = pygame.image.load('sprites.png').convert_alpha()
 sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
 
 BG = (50, 50, 50)
@@ -14,39 +19,47 @@ animation_list = []
 animation_steps = [4,6,3,4]
 action = 0
 last_update = pygame.time.get_ticks()
-animation_cooldown=500
+animation_cooldown=250
 frame = 0
+step_counter = 0
 
-for x in range(animation_steps):
-    animation_list.append(sprite_sheet.get_image(0,24,24,3,BLACK))
+for animation in animation_steps:
+	temp_img_list = []
+	for _ in range(animation):
+		temp_img_list.append(sprite_sheet.get_image(step_counter,40,70,1,BLACK))
+		step_counter += 1
+	animation_list.append(temp_img_list)
 
 run = True
 while run:
 
 	#update background
-	screen.fill(BG)
+	screen.fill((BG))
 
     #update animation
-current_time = pygame.time.get_ticks()
-if current_time-last_update>=animation_cooldown:
-    frame += 1
-    last_update = current_time
-if frame >= len(animation_list):
-		frame=0
+	current_time = pygame.time.get_ticks()
+	if current_time-last_update>=animation_cooldown:
+			frame += 1
+			last_update = current_time
+			if frame >= len(animation_list[action]):
+				frame=0
 
 
 	#show frame image
-screen.blit(animation_list[frame], (0,0))
+	screen.blit(animation_list[action][frame], (0,0))
 
 	#event handler
-for event in pygame.event.get():
-	if event.type == pygame.QUIT:
-		run = False
-	if event.type == pygame.KEYDOWN:
-		if event.key == pygame.K_DOWN and action >0:
-            action -= 1
-            frame = 1
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_DOWN and action >0:
+					action += 1
+					frame = 0
+			if event.key == pygame.K_UP and action < len(animation_list) -1:
+					action +=1
+					frame = 0
 
-pygame.display.update()
+	pygame.display.update()
 
 pygame.quit()
